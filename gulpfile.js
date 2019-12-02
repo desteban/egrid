58151abd4133fd8d5fe4e4da951747c6d0016387
desteban <esteban28c@hotmail.com>
2019-12-02 18:00:38 -0500

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+const sass = require('gulp-sass');
+const autoPrefixer = require('gulp-autoprefixer');
+
+//tareas
+
+function css() {
+	return gulp
+		.src(['src/sass/index.scss'])
+		.pipe(sass().on('error', sass.logError))
+		.pipe(sass({ outputStyle: 'compressed' }))
+		.pipe(
+			autoPrefixer({
+				versions: ['last 2 browsers']
+			})
+		)
+		.pipe(gulp.dest('dist/css'));
+}
+
+function js() {
+	return gulp
+		.src(['node_modules/headroom.js/dist/headroom.js', 'src/js/index.js'])
+		.pipe(gulp.dest('dist/js'));
+}
+
+function font_awesome() {
+	return gulp
+		.src(['node_modules/font-awesome/css/font-awesome.min.css'])
+		.pipe(gulp.dest('dist/css'));
+}
+
+function fonts() {
+	return gulp.src('node_modules/font-awesome/fonts/*').pipe(gulp.dest('dist/fonts'));
+}
+
+gulp.task('default', () => {
+	css();
+	js();
+	font_awesome();
+	fonts();
+	//gulp.watch('src/sass/*.scss').on('change', css());
+	gulp.watch('src/sass/*.scss', gulp.series(css));
+	gulp.watch('src/js/*.js', gulp.series(js));
+
+	gulp.watch('dist/index.html').on('change', browserSync.reload);
+	gulp.watch('dist/css/**').on('change', browserSync.reload);
+	gulp.watch('dist/js/**').on('change', browserSync.reload);
+
+	browserSync.init({
+		server: { baseDir: 'dist' }
+	});
+});
---
+++ src/js/index.js
@@ -0,0 +1,1 @@
+require('headroom.js/dist/headroom');
