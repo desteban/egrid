cf313945d5afb232e9a1ecaaa0e52a348f42468b
desteban <esteban28c@hotmail.com>
2019-12-03 16:18:59 -0500

    html

--- webpack.config.js
+++ webpack.config.js
@@ -7,11 +7,19 @@ module.exports = {
 	},
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'app.js'
+		filename: 'js/app.js'
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			template: './src/index.html'
+			template: './src/index.html',
+			minify: {
+				collapseWhitespace: true,
+				removeComments: true,
+				removeRedundantAttributes: true,
+				removeScriptTypeAttributes: true,
+				removeStyleLinkTypeAttributes: true,
+				useShortDoctype: true
+			}
 		})
 	]
 };
