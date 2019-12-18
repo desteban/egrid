9011350f72767d948233354d51595634e4dce978
desteban <esteban28c@hotmail.com>
2019-12-18 18:58:30 -0500

    animacion del label para los inputs

--- src/js/form.js
+++ src/js/form.js
@@ -1,5 +1,24 @@
 (function() {
-	//obtener formulario
-	let formulario = document.getElementsByClassName('form'),
-		elemento = formulario.elements;
+	//obtener input
+	let inputs = document.querySelectorAll('input');
+
+	var focusInput = function() {
+		this.parentElement.children[1].className = 'label active';
+	};
+
+	var blurInput = function() {
+		this.parentElement.children[1].className = 'label';
+	};
+
+	for (let i = 0; i < inputs.length; i++) {
+		//escojer los inputs de tipo text, password, email
+		if (
+			inputs[i].type == 'text' ||
+			inputs[i].type == 'password' ||
+			inputs[i].type == 'email'
+		) {
+			inputs[i].addEventListener('focus', focusInput);
+			inputs[i].addEventListener('blur', blurInput);
+		}
+	}
 })();
