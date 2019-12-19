493c6ef8ba5a04f4890951908f18c39adbbdfd75
desteban <esteban28c@hotmail.com>
2019-12-18 19:24:06 -0500

    validar campos requeridos

--- src/js/form.js
+++ src/js/form.js
@@ -4,19 +4,32 @@
 
 	var focusInput = function() {
 		this.parentElement.children[1].className = 'label active';
+		this.parentElement.children[0].className = this.parentElement.children[0].className.replace(
+			'error',
+			''
+		);
 	};
 
 	var blurInput = function() {
-		this.parentElement.children[1].className = 'label';
+		if (this.value <= 0) {
+			this.parentElement.children[1].className = ' label';
+			// buscar si el campo es necesario
+			let complete = this.parentElement.children[0].classList;
+			for (let i = 0; i < complete.length; i++) {
+				if (complete[i] == 'require') {
+					console.log('require');
+					this.parentElement.children[0].className =
+						this.parentElement.children[0].className + ' error';
+				}
+			}
+		} else {
+			this.parentElement.children[1].className = ' label active';
+		}
 	};
 
 	for (let i = 0; i < inputs.length; i++) {
 		//escojer los inputs de tipo text, password, email
-		if (
-			inputs[i].type == 'text' ||
-			inputs[i].type == 'password' ||
-			inputs[i].type == 'email'
-		) {
+		if (inputs[i].type == 'text' || inputs[i].type == 'password' || inputs[i].type == 'email') {
 			inputs[i].addEventListener('focus', focusInput);
 			inputs[i].addEventListener('blur', blurInput);
 		}
